e842241d95a7f90ccc756e57445e56776c7315da
gebrur24 <105764361+gebrur24@users.noreply.github.com>
2022-06-05 20:05:49 -0500

    Add files via upload

---
+++ guessingagegame.pseuodocode.py
@@ -0,0 +1,20 @@
+#Ruth Gebru
+#pseuodoode
+    #start
+    #input instrutions
+    #input list of words (colors)
+    #import os and random
+    #get loop
+    #get boarders
+    #print instrutions
+    #get list of colors
+    #get list of colors
+    #get randomizer to select a random color from the list 
+    #import use input to guess a color 
+    #print "you got it right" if the guess was correct
+    # input break if the guess was right
+    # print "you got the wrong color" if the guess was wrong 
+    # input break if the user wants to stop playing 
+    # #trend
+import random
+import os  
